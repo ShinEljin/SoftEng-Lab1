1c846a463df6c0e8002efa73c193ce2bbdb7e525
Raphael <psyruz18@gmail.com>
2022-11-13 20:35:15 +0800

    change

--- server.js
+++ server.js
@@ -260,10 +260,8 @@ app.post("/otp-login", (req, res) => {
   }
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
+let port = process.env.PORT || 3000;
+
 app.listen(port, () => {
   console.log("Server is running or port 3000");
 });
